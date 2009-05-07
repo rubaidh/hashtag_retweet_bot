c7ace3014a05f57e9978ecd09b0c6fc30174caa4
Mark Connell <mark@rubaidh.com>
2009-05-07 22:41:06 +0100

    Drop in changes that were made on server (yes I should be shot for doing that)

--- bot.rb
+++ bot.rb
@@ -7,7 +7,7 @@ require 'feedzirra' # feed helper
 ActiveRecord::Base.establish_connection(
   :adapter => "mysql",
   :username => "root",
-  :host => "127.0.0.1",
+  :host => "localhost",
   :password => "",
   :database => "twibot"
 )
@@ -19,49 +19,45 @@ end
 ###
 #  What we want the bot to do
 ###
-#  1. Listen to an rss feed and store that stuff 
+#  1. Listen to an rss feed and store that stuff
 #  2. Work out which tweets need to be tweeted by the bot
 #  3. send the tweets and mark them as 'tweeted'
 #
 
 feed_thread = Thread.new do
   while(true != false)
-    # fetch the feed
-    feed = Feedzirra::Feed.fetch_and_parse("http://search.twitter.com/search.atom?q=+%23sor09")
+begin
+      # fetch the feed
+      feed = Feedzirra::Feed.fetch_and_parse("http://search.twitter.com/search.atom?q=+%23sor09")
 
-    feed.entries.each do |entry|
-      tweet = Tweets.find_or_create_by_twitter_id(
-                :twitter_id => entry.id,
-                :published  => entry.published,
-                :title      => entry.title
-              )
+      feed.entries.reverse.each do |entry|
+        tweet = Tweets.find_or_create_by_twitter_id(
+                  :twitter_id => entry.id,
+                  :published  => entry.published,
+                  :title      => entry.title,
+                  :content    => entry.content,
+                  :link       => entry.url
+                )
 
-      if tweet.tweeted.blank?
-        post_tweet "#{tweet.twitter_id}"
-        tweet.update_attribute(:tweeted, true)
+        if tweet.tweeted.blank?
+          #post_tweet "#{tweet.link}"
+          origin = tweet.link.gsub(/^http.*com\//,"").gsub(/\/statuses\/\d*/,"")
+          message = tweet.title.gsub(/#sor09/i, "")
+          if origin.size + message.size  <= 135
+            twitter.status(:post, "RT @#{origin}: #{message}")
+          else
+            twitter.status(:post, "RT @#{origin} tagged 'sor09':
+  #{tweet.link}")
+          end
+          #, tweet.link.gsub(/^http.*statuses\//, ""))
+          puts "#{Time.now.to_s(:long)}"
+          tweet.update_attribute(:tweeted, true)
+        end
       end
-    end
-
+  rescue
+  end
+    sleep(60)
   end
 end
 
-
-# Receive messages, and tweet them publicly
-#
-#message do |message, params|
-#  post_tweet message
-#end
-
-# Respond to @replies if they come from the right crowd
-#
-#reply :from => [:cjno, :irbno] do |message, params|
-#  post_tweet "@#{message.sender.screen_name} I agree"
-#end
-
-# Listen in and log tweets
-#
-tweet do |message, params|
-  MyApp.log_tweet(message)
-end
-
-
+feed_thread.join
